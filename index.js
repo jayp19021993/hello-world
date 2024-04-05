67bd112fca2df75430e5aa0d70a6d4c22320db92
Jay Patel <jay_patel_123@hotmail.co.uk>
2024-04-05 15:53:36 +0100

    change to the js file

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 //console.log("Hello, World!"); 
 
-//object.onclick = function(darkMode){let element = document.body;
-    //element.classList.toggle(".dark-mode");
-  //};    
-
   function darkMode(){
    document.body.style.background="black"; 
    document.querySelectorAll('h1').forEach(e=>e.style.color="white");
