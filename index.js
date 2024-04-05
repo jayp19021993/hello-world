4151780e945f39a4d62064ba73b62e55cb63286a
Jay Patel <jay_patel_123@hotmail.co.uk>
2024-04-05 14:23:51 +0100

    made chane to function darkMode

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 //console.log("Hello, World!"); 
 
-object.onclick = function(darkMode){let element = document.body;
-    element.classList.toggle(".dark-mode");
-  };    
+//object.onclick = function(darkMode){let element = document.body;
+    //element.classList.toggle(".dark-mode");
+  //};    
+
+  function darkMode(){
+    
+  }
