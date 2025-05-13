84e90ba5ee64b2276a3cc8875488332ddb7d5b3b
Jay Patel <jay_patel_123@hotmail.co.uk>
2025-05-13 12:49:09 +0100

    Add add a dark mode to p tag

--- index.js
+++ index.js
@@ -3,9 +3,11 @@
   function darkMode(){
    document.body.style.background="black"; 
    document.querySelectorAll('h1').forEach(e=>e.style.color="white");
+   document.querySelectorAll('p').forEach(e=>e.style.color="white");
   } 
 
   function lightMode(){
     document.body.style.background="white"; 
     document.querySelectorAll('h1').forEach(e=>e.style.color="black");
+    document.querySelectorAll('p').forEach(e=>e.style.color="black");
    }
