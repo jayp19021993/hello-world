b16c594d93f3f7a9a90e0f6581bcef442860b7c0
Jay Patel <jay_patel_123@hotmail.co.uk>
2025-06-24 11:16:55 +0100

    cometd out function to make a new one for dark mode

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 //console.log("Hello, World!"); 
 
-  function darkMode(){
-   document.body.style.background="black"; 
-   document.querySelectorAll('h1').forEach(e=>e.style.color="white");
-   document.querySelectorAll('p').forEach(e=>e.style.color="white");
-  } 
+  //function darkMode(){
+   //document.body.style.background="black"; 
+   //document.querySelectorAll('h1').forEach(e=>e.style.color="white");
+   //document.querySelectorAll('p').forEach(e=>e.style.color="white");
+  //} 
 
-  function lightMode(){
-    document.body.style.background="white"; 
-    document.querySelectorAll('h1').forEach(e=>e.style.color="black");
-    document.querySelectorAll('p').forEach(e=>e.style.color="black");
-   }
+  //function lightMode(){
+  //  document.body.style.background="white"; 
+  //  document.querySelectorAll('h1').forEach(e=>e.style.color="black");
+  //  document.querySelectorAll('p').forEach(e=>e.style.color="black");
+  // }
+
+  function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+} 
