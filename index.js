7e0b7c653700af25636c879e8b79b806ce7d3e9a
Jay Patel <jay_patel_123@hotmail.co.uk>
2025-06-24 11:30:51 +0100

    changes to functions

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 //console.log("Hello, World!"); 
 
-  //function darkMode(){
-   //document.body.style.background="black"; 
-   //document.querySelectorAll('h1').forEach(e=>e.style.color="white");
-   //document.querySelectorAll('p').forEach(e=>e.style.color="white");
-  //} 
+  function darkMode(){
+   document.body.style.background="black"; 
+   document.querySelectorAll('h1').forEach(e=>e.style.color="white");
+   document.querySelectorAll('p').forEach(e=>e.style.color="white");
+  } 
 
-  //function lightMode(){
-  //  document.body.style.background="white"; 
-  //  document.querySelectorAll('h1').forEach(e=>e.style.color="black");
-  //  document.querySelectorAll('p').forEach(e=>e.style.color="black");
-  // }
-
-  function myFunction() {
-  let element = document.body;
-  element.classList.toggle("dark-mode");
-} 
+  function lightMode(){
+    document.body.style.background="white"; 
+    document.querySelectorAll('h1').forEach(e=>e.style.color="black");
+    document.querySelectorAll('p').forEach(e=>e.style.color="black");
+  }
