bdec63e05ebc1aa6ecf49a4c21d8bfe83b0ade43
Jay Patel <jay_patel_123@hotmail.co.uk>
2025-06-26 19:04:43 +0100

    add the dark mode

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 //console.log("Hello, World!"); 
 
-  function darkMode(){
-   document.body.style.background="black"; 
-   document.querySelectorAll('h1').forEach(e=>e.style.color="white");
-   document.querySelectorAll('p').forEach(e=>e.style.color="white");
-  } 
+   //function darkMode(){
+   //document.body.style.background="black"; 
+   //document.querySelectorAll('h1').forEach(e=>e.style.color="white");
+   //document.querySelectorAll('p').forEach(e=>e.style.color="white");
+  //} 
 
-  function lightMode(){
-    document.body.style.background="white"; 
-    document.querySelectorAll('h1').forEach(e=>e.style.color="black");
-    document.querySelectorAll('p').forEach(e=>e.style.color="black");
-  }
+  //function lightMode(){
+   //document.body.style.background="white"; 
+   //document.querySelectorAll('h1').forEach(e=>e.style.color="black");
+   //document.querySelectorAll('p').forEach(e=>e.style.color="black");
+  //}
+
+  let darkmode = localStorage.getItem('darkmode')
+const themeSwitch = document.getElementById('theme-switch')
+
+const enableDarkmode = () => {
+  document.body.classList.add('darkmode')
+  localStorage.setItem('darkmode', 'active')
+}
+
+const disableDarkmode = () => {
+  document.body.classList.remove('darkmode')
+  localStorage.setItem('darkmode', null)
+}
+
+if(darkmode === "active") enableDarkmode()
+
+themeSwitch.addEventListener("click", () => {
+  darkmode = localStorage.getItem('darkmode')
+  darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+})
